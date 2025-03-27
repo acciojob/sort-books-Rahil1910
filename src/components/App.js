fc2536c4443f09cfd50fc5cf4286f22ef925a624
Rahil1910 <rahilrafique1234@gmail.com>
2025-03-27 22:19:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,11 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <>
+      <Tables/>
+    </>
   )
 }
 
